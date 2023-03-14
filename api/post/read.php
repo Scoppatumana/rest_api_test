414512d1bd8a7eac20eb10ea95fde404bb6352c8
Scoppatumana <boluwasodiq314@gmail.com>
2023-03-14 09:57:50 +0100

    ddddddd

--- api/post/read.php
+++ api/post/read.php
@@ -1,45 +1,54 @@
-<?php
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
+  include_once '../../config/Database.php';
+  include_once '../../models/Post.php';
 
-include_once '../../config/Database.php';
-include_once '../../module/Post.php';
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
 
-$database = new Database;
-$db = $database->connect();
+  // Instantiate blog post object
+  $post = new Post($db);
 
-$post = new Post($db);
-$result = $post->read();
+  // Blog post query
+  $result = $post->read();
+  // Get row count
+  $num = $result->rowCount();
 
-$num = $result->rowCount();
-
-if($num > 0){
+  // Check if any posts
+  if($num > 0) {
+    // Post array
     $posts_arr = array();
-    $posts_arr['data'] = array();
+    // $posts_arr['data'] = array();
 
     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
+      extract($row);
 
-        $post_item = array(
-            'id'=> $id,
-            'title'=> $title,
-            'body' => html_entity_decode($body),
-            'author' => $author,
-            'category_id' => $category_id,
-            'category_name' => $category_name
-        );
+      $post_item = array(
+        'id' => $id,
+        'title' => $title,
+        'body' => html_entity_decode($body),
+        'author' => $author,
+        'category_id' => $category_id,
+        'category_name' => $category_name
+      );
 
-        array_push($posts_arr['data'], $post_item);
+      // Push to "data"
+      array_push($posts_arr, $post_item);
+      // array_push($posts_arr['data'], $post_item);
     }
 
-    // turn to json
-
+    // Turn to JSON & output
     echo json_encode($posts_arr);
-}else{
-    echo json_encode(
-        array('message' => 'No Post Found')
-    );
-}
 
-?>
+  } else {
+    // No Posts
+    echo json_encode(
+      array('message' => 'No Posts Found')
+    );
+  }
+
+  ?>
--- module/Post.php
+++ module/Post.php
@@ -1,49 +1,39 @@
-<?php
-    class Post {
-        private $conn;
-        private $table = "posts";
+<?php 
+  class Post {
+    // DB stuff
+    private $conn;
+    private $table = 'posts';
 
-        // Post Properties
-        public $id;
-        public $category_id;
-        public $category_name;
-        public $title;
-        public $body;
-        public $author;
-        public $created_at;
+    // Post Properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $created_at;
 
-        // constructor with db
-
-        public function __construct($db){
-            $this->conn = $db;
-        }
-
-        // get posts
-
-       public function read(){
-        $query = 'SELECT
-        c.name as category_name,
-        p.id,
-        p.category_id,
-        p.title,
-        p.body,
-        p.author,
-        p.created_at
-        FROM
-            ' . $this->table .' p
-            LEFT JOIN
-            categories c ON p.category_id = c.id
-            ORDER by
-            p.created_at DESC';
-
-            // Prepare the Statement
-            $stmt = $this->conn->prepare($query);
-
-            // Execute 
-            $stmt->execute();
-
-            return $stmt;
-       }
+    // Constructor with DB
+    public function __construct($db) {
+      $this->conn = $db;
     }
 
+    // Get Posts
+    public function read() {
+      // Create query
+      $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+                                FROM ' . $this->table . ' p
+                                LEFT JOIN
+                                  categories c ON p.category_id = c.id
+                                ORDER BY
+                                  p.created_at DESC';
+      
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Execute query
+      $stmt->execute();
+
+      return $stmt;
+    }
 ?>
